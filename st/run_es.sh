67fc512572e1841e31dc7e359c0453adfd78c2ab
jiyinhe <jiyinhe@gmail.com>
2014-07-04 13:58:43 +0200

    new file:   run_es.sh

---
+++ st/run_es.sh
@@ -0,0 +1,14 @@
+# Run elastic search service wrapper.
+# Note: the wrapper is not included in the elsaticsearch itself.
+# It can be obtained at:
+# https://github.com/elasticsearch/elasticsearch-servicewrapper.git
+#
+# The wrapper contains one directory called service,
+# and it needs to be placed under:
+# elasticsearch-*.*/bin/
+
+# You can change the path to where you prefer to 
+# install elastic search, and put the wrapper 
+# in the corresponding location.
+
+es/elasticsearch-1.1.1/bin/service/elasticsearch $1
