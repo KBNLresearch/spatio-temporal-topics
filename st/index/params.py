0421800de8c3319419e8a1f8cf082cfa6029f9f5
jiyinhe <jiyinhe@gmail.com>
2014-07-04 15:30:29 +0200

    new file:   index/params.py

---
+++ st/index/params.py
@@ -0,0 +1,12 @@
+"""
+Set the paramters for indexing
+"""
+# URL of the elastic search service
+# Default: localhost:9200
+ES_URL = 'localhost:9200'
+
+# Cluster name
+
+
+# Name of the index
+# We only create one index to store everything
