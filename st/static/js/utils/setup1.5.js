7ee7beac1ab9e8649a47390d22a485949e77f204
jiyinhe <jiyinhe@gmail.com>
2014-08-28 11:01:42 +0200

    new file:   static/js/utils/setup1.4.js 	new file:   static/js/utils/setup1.5.js

---
+++ st/static/js/utils/setup1.5.js
@@ -0,0 +1,17 @@
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+
+$(function(){
+var csrftoken = $.cookie('csrftoken');
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+});
